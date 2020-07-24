e82f03acaa27c7116691b99592ca8e62b7b31f1e
VictorJeanJr <54330872+VictorJeanJr@users.noreply.github.com>
2020-07-24 15:44:24 -0300

    update

--- src/main/java/dev/victorjeanjr/custominventory/CustomInventory.java
+++ src/main/java/dev/victorjeanjr/custominventory/CustomInventory.java
@@ -1,7 +1,6 @@
 package dev.victorjeanjr.custominventory;
 
 import dev.victorjeanjr.custominventory.content.InventoryContains;
-import dev.victorjeanjr.custominventory.tester.ListenerTest;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,8 +9,6 @@ public class CustomInventory extends JavaPlugin {
     @Override
     public void onEnable() {
         InventoryContains.init(this);
-
-        Bukkit.getPluginManager().registerEvents(new ListenerTest(), this);
 
         System.out.println("Started with success.");
     }
